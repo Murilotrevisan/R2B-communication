2409a7e627434a0c5b0cf4103c9495fa54a92403
murilopasini <murilopasini@usp.br>
2022-06-05 12:37:04 -0300

    mac finder interface update

--- ESP-NOW-R2B/MAC-addr-finder/MAC-addr-finder.ino
+++ ESP-NOW-R2B/MAC-addr-finder/MAC-addr-finder.ino
@@ -1,11 +1,13 @@
-// Complete Instructions to Get and Change ESP MAC Address: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
-
 #include "WiFi.h"
  
 void setup(){
   Serial.begin(115200);
   WiFi.mode(WIFI_MODE_STA);
+  Serial.println("----------------------------------------------")
+  Serial.println("Bem vindo ao identificador de MAC addres da sua ESP")
+  Serial.print("\r\nSeu endereço MAC é: \t")
   Serial.println(WiFi.macAddress());
+  Serial.println("----------------------------------------------")
 }
  
 void loop(){
